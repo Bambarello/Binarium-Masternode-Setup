c38935f89928b35383c0d184b5d2519ca9b01483
Alex <33266159+Bambarello@users.noreply.github.com>
2018-10-03 00:12:47 +0300

    Update binarium_install_sentinel.sh

--- binarium_install_sentinel.sh
+++ binarium_install_sentinel.sh
@@ -276,7 +276,9 @@ function install_sentinel() {
   #setup config
   echo "dash_conf=$CONFIGFOLDER/$CONFIG_FILE" >> $CONFIGFOLDER/sentinel/sentinel.conf 
   #setup cron
+  if grep '*/5 * * * * cd $CONFIGFOLDER/sentinel && ./venv/bin/python bin/sentinel.py' /etc/crontab; then
   (crontab -l 2>/dev/null; echo "*/5 * * * * cd $CONFIGFOLDER/sentinel && ./venv/bin/python bin/sentinel.py 2>&1 >> $CONFIGFOLDER/sentinel-cron.log") | crontab -
+  fi
 }
 
 function important_information() {
