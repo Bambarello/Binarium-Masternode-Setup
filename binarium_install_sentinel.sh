52456da549ac688bc08a6875a3ee827820b19624
Alex <33266159+Bambarello@users.noreply.github.com>
2018-10-06 17:27:54 +0300

    Update binarium_install_sentinel.sh

--- binarium_install_sentinel.sh
+++ binarium_install_sentinel.sh
@@ -262,7 +262,7 @@ if [ "$?" -gt "0" ];
     echo "apt-get update"
     echo "apt-get install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget htop pwgen curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5 unzip p7zip-full${NC}"
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5 unzip p7zip-full"
  exit 1
 fi
 clear
