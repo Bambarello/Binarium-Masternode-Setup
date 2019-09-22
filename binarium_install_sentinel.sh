be3e71dc73a232d6b2e8ab7a694224bfa21bad87
Alex <33266159+Bambarello@users.noreply.github.com>
2019-09-22 16:13:13 +0300

    Update binarium_install_sentinel.sh

--- binarium_install_sentinel.sh
+++ binarium_install_sentinel.sh
@@ -278,8 +278,10 @@ fi
 
 function checks() {
 if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
+  if [[ $(lsb_release -d) != *18.04* ]]; then
+    echo -e "${RED}You are not running Ubuntu 16.04 or 18.04. Installation is cancelled.${NC}"
+    exit 1
+  fi
 fi
 
 if [[ $EUID -ne 0 ]]; then
