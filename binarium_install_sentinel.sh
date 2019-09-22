195cfb75bb818640b743896d98d56bd652b06154
Alex <33266159+Bambarello@users.noreply.github.com>
2019-09-22 16:21:01 +0300

    Update binarium_install_sentinel.sh

--- binarium_install_sentinel.sh
+++ binarium_install_sentinel.sh
@@ -316,7 +316,7 @@ sudo apt-get update -y >/dev/null 2>&1
 sudo apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget htop pwgen curl nano libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ libzmq5 unzip p7zip-full >/dev/null 2>&1
+libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ libzmq5 unzip p7zip-full net-tools >/dev/null 2>&1
 sudo apt-get autoclean && sudo apt-get autoremove >/dev/null 2>&1
 
 # Checking fail2ban
